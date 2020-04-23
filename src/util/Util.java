c5a89639367eb86e27f9770946b105d4b6666aa7
DenApache <DenApache@users.noreply.github.com>
2020-04-23 15:53:09 +0200

    Ajout du calcule de la distance entre deux case + listePersonnages

--- src/util/Util.java
+++ src/util/Util.java
@@ -2,8 +2,13 @@ package util;
 
 import java.util.ArrayList;
 
+<<<<<<< Upstream, based on branch 'dev' of https://github.com/Floliroy/MegaChest.git
 import personnages.Personnage;
 import plateau.Plateau;
+=======
+import personnages.*;
+import plateau.Case;
+>>>>>>> 98d77ee Ajout du calcule de la distance entre deux case + listePersonnages
 
 public class Util {
 	
@@ -26,6 +31,7 @@ public class Util {
 		return retour;
 	}
 	
+<<<<<<< Upstream, based on branch 'dev' of https://github.com/Floliroy/MegaChest.git
 	/**
 	 * Renvoit la liste des personnages avec leurs coordonnées
 	 * @param equipe La liste des personnages
@@ -43,4 +49,32 @@ public class Util {
 		return retour;
 	}
 	
+=======
+	public static ArrayList<Personnage> listePersonnages(){
+		
+		ArrayList<Personnage> allPersonnages = new ArrayList<Personnage>();
+		
+		allPersonnages.add(new Ahri());
+		allPersonnages.add(new Fizz());
+		allPersonnages.add(new Illaoi());
+		allPersonnages.add(new Jarvan());
+		allPersonnages.add(new Kayle());
+		allPersonnages.add(new Kled());
+		allPersonnages.add(new Poppy());
+		allPersonnages.add(new Pyke());
+		allPersonnages.add(new Quinn());
+		allPersonnages.add(new Shyvana());
+		allPersonnages.add(new Sion());
+		allPersonnages.add(new Sona());
+		allPersonnages.add(new Swain());
+		allPersonnages.add(new Talon());
+		allPersonnages.add(new Wukong());
+		
+		return allPersonnages;
+	}
+	
+	public static int distanceCase(Case depart, int positionXDestination, int positionYDestination) {
+		return Math.abs(depart.getPositionX() - positionXDestination) + Math.abs(depart.getPositionY() - positionYDestination);
+	}
+>>>>>>> 98d77ee Ajout du calcule de la distance entre deux case + listePersonnages
 }
