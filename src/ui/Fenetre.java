a45daf842fa809b01ae25b86995a28c08d0087db
DenApache <DenApache@users.noreply.github.com>
2020-05-13 23:28:55 +0200

    Ajout début affichage des actions dans le panneau de log

--- src/ui/Fenetre.java
+++ src/ui/Fenetre.java
@@ -1,15 +1,21 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.io.PrintStream;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 import partie.Jeu;
+import util.RedirectionOutput;
 
 public class Fenetre extends JFrame {
 
@@ -30,17 +36,46 @@ public class Fenetre extends JFrame {
 		
 		panneauInfos = new PanneauInfos();
 		panneauLogs = new JPanel();
-		panneauLogs.setBackground(Color.CYAN);
+		
 		panneauActions = new JPanel();
 		panneauActions.setBackground(Color.YELLOW);
 		
 		
-		JInternalFrame test = new JInternalFrame();
-		test.add(panneauInfos);
-		BasicInternalFrameUI bi = (BasicInternalFrameUI) test.getUI();
-		bi.setNorthPane(null);
-	
-		test.setVisible(true);
+		JInternalFrame iFrameInfos = new JInternalFrame();
+		iFrameInfos.add(panneauInfos);
+		iFrameInfos.setBorder(null);
+		BasicInternalFrameUI biFrameInfos = (BasicInternalFrameUI) iFrameInfos.getUI();
+		biFrameInfos.setNorthPane(null);
+		iFrameInfos.setVisible(true);
+		
+		
+		
+		
+		
+		
+		JInternalFrame iFrameLogs= new JInternalFrame();
+		JPanel test = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		JTextArea textArea= new JTextArea();
+		textArea.setEditable(false);
+		textArea.setBackground(Color.GREEN);
+		
+		
+		PrintStream printStream = new PrintStream(new RedirectionOutput(textArea));
+		
+		PrintStream standartOut = System.out;
+		
+		System.setOut(printStream);
+		//System.setErr(printStream);
+		
+		test.setBackground(Color.GREEN);
+		JScrollPane scroll = new JScrollPane(textArea);
+		scroll.setPreferredSize(new Dimension(100, 100));
+		scroll.setBorder(null);
+		test.add(scroll);
+		iFrameLogs.add(test);
+		iFrameLogs.setResizable(false);
+		iFrameLogs.setVisible(true);
+		
 		
 		
 		panneauJeu = new PanneauJeu(jeu, panneauInfos);
@@ -63,18 +98,18 @@ public class Fenetre extends JFrame {
 		cons.gridy = 0;
 		cons.gridheight = 1;
 		cons.weightx = 0.3;
-		add(test, cons);
+		add(iFrameInfos, cons);
 
-		cons.gridwidth = 1;
-		cons.weighty = 0.34;
 		cons.gridx = 0;
 		cons.gridy = 1;
+		cons.gridwidth = 1;
+		cons.weighty = 0.34;
 		add(panneauActions, cons);
 		
 		cons.gridwidth = 1;
 		cons.gridx = 1;
 		cons.gridy = 1;
-		add(panneauLogs, cons);
+		add(iFrameLogs, cons);
 	}
 	
 	public void addPanel(JPanel panneau) {
