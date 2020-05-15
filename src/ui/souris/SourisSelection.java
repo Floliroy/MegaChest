09321c4c22a2eaa59ebab57cf6b6d90c97aab40e
Flo Leeroy <florian.leeroy@hotmail.com>
2020-05-15 11:48:36 +0200

    fix enlever personnage si equipe complete

--- src/ui/souris/SourisSelection.java
+++ src/ui/souris/SourisSelection.java
@@ -32,36 +32,34 @@ public class SourisSelection extends MouseAdapter{
 		Equipe equipe = fenetre.getJeu().getJoueurActif().getEquipe();
 		Plateau plateau = fenetre.getJeu().getPlateauJeu();
 
-		if(equipe.getListePersonnages().size() < Equipe.TAILLE_EQUIPE) {
-			if(equipe.getListePersonnages().contains(personnage)) {
-				System.out.println("Suppression de " + personnage.getNom());
-				caseImage.setTransparency(null);
-				caseImage.repaint();
-				equipe.removeEquipe(personnage);
-				panneauJeu.setPersonnageSelectionne(null);
-				Case previousCase = plateau.getCase(personnage);
-				previousCase.setPersonnage(null, null);
-				previousCase.getPanel().setTransparency(null);
-				previousCase.getPanel().repaint();
-				
+		if(equipe.getListePersonnages().contains(personnage)) {
+			System.out.println("Suppression de " + personnage.getNom());
+			caseImage.setTransparency(null);
+			caseImage.repaint();
+			equipe.removeEquipe(personnage);
+			panneauJeu.setPersonnageSelectionne(null);
+			Case previousCase = plateau.getCase(personnage);
+			previousCase.setPersonnage(null, null);
+			previousCase.getPanel().setTransparency(null);
+			previousCase.getPanel().repaint();
+			
+		}else if(equipe.getListePersonnages().size() < Equipe.TAILLE_EQUIPE){
+			System.out.println("Ajout de " + personnage.getNom());
+			caseImage.setTransparency(Util.getGrayTransparency());
+			caseImage.repaint();
+			equipe.addEquipe(personnage);
+			Case newCase;
+			String fond;
+			if(fenetre.getJeu().getJoueurActif().equals(fenetre.getJeu().getJoueur1())) {
+				newCase = plateau.getFirstCaseLeft();
+				fond = "bleu.png";
 			}else {
-				System.out.println("Ajout de " + personnage.getNom());
-				caseImage.setTransparency(Util.getGrayTransparency());
-				caseImage.repaint();
-				equipe.addEquipe(personnage);
-				Case newCase;
-				String fond;
-				if(fenetre.getJeu().getJoueurActif().equals(fenetre.getJeu().getJoueur1())) {
-					newCase = plateau.getFirstCaseLeft();
-					fond = "bleu.png";
-				}else {
-					newCase = plateau.getFirstCaseRight();
-					fond = "rouge.png";
-				}
-				newCase.setPersonnage(personnage, fond);
-				newCase.getPanel().setTransparency(null);
-				newCase.getPanel().repaint();
+				newCase = plateau.getFirstCaseRight();
+				fond = "rouge.png";
 			}
+			newCase.setPersonnage(personnage, fond);
+			newCase.getPanel().setTransparency(null);
+			newCase.getPanel().repaint();
 		}
 		if(equipe.getListePersonnages().size() >= Equipe.TAILLE_EQUIPE) {
 			System.out.println("Votre equipe est complete, n'oubliez pas de repositionnez vos personnages.");
